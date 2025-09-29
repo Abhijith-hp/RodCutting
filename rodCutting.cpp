c9f769a587ffccea70da592a7e8caeead777ff9b
abhijith_g1 <abhijith_g@geojit.com>
2025-09-29 10:26:53 +0530

    Implemented the rodCutting problem of leetcode with tabulation

--- rodCutting.cpp
+++ rodCutting.cpp
@@ -3,25 +3,22 @@
 class Solution {
   public:
   
-    int selections(int index,int n,vector<int>&price,vector<vector<int>>&dp){
-        
-        if(index==0){
-            return n * price[0];
-        }
-        if (dp[index][n]!=-1) return dp[index][n];
-        
-        int nonTake = 0+selections(index-1,n,price,dp);
-        int take = INT_MIN;
-        int rodLength = index+1;
-        if(rodLength<=n)
-            take = price[index]+selections(index,n-rodLength,price,dp);
-        dp[index][n] = max(take,nonTake);
-        return dp[index][n];
-    }
     int cutRod(vector<int> &price) {
         int size = price.size();
         vector<vector<int>>dp(size,vector<int>(size+1,-1));
-        return selections(price.size()-1,price.size(),price,dp);
+        for(int i=0;i<=size;i++)
+            dp[0][i] = i*price[0];
+        for(int i=1;i<size;i++){
+            for(int j=0;j<=size;j++){
+        int nonTake = 0+dp[i-1][j];
+        int take = INT_MIN;
+        int rodLength = i+1;
+        if(rodLength<=j)
+            take = price[i]+dp[i][j-rodLength];
+        dp[i][j] = max(take,nonTake); 
+            }
+        }
+        return dp[size-1][size];
         
     }
 };
