aa99ac3d1ef4a69d01b748dcf8083b7520d64774
abhijith_g1 <abhijith_g@geojit.com>
2025-09-29 10:27:23 +0530

    Implemented the rodCutting problem of leetcode with tabulation by optimizing space

--- rodCutting.cpp
+++ rodCutting.cpp
@@ -1,4 +1,4 @@
-
+// User function Template for C++
 
 class Solution {
   public:
@@ -6,19 +6,22 @@ class Solution {
     int cutRod(vector<int> &price) {
         int size = price.size();
         vector<vector<int>>dp(size,vector<int>(size+1,-1));
+        vector<int>prev(size+1,0);
+        vector<int>curr(size+1,0);
         for(int i=0;i<=size;i++)
-            dp[0][i] = i*price[0];
+            prev[i] = i*price[0];
         for(int i=1;i<size;i++){
             for(int j=0;j<=size;j++){
-        int nonTake = 0+dp[i-1][j];
+        int nonTake = 0+prev[j];
         int take = INT_MIN;
         int rodLength = i+1;
         if(rodLength<=j)
-            take = price[i]+dp[i][j-rodLength];
-        dp[i][j] = max(take,nonTake); 
+            take = price[i]+curr[j-rodLength];
+        curr[j] = max(take,nonTake); 
             }
+            prev=curr;
         }
-        return dp[size-1][size];
+        return prev[size];
         
     }
 };
