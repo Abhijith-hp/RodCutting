3a3dfa5d7e0c1e94dffee5105d52bee34f35612d
abhijith_g1 <abhijith_g@geojit.com>
2025-09-29 10:26:23 +0530

    Implemented the rodCutting problem of leetcode using memozation

--- rodCutting.cpp
+++ rodCutting.cpp
@@ -3,21 +3,25 @@
 class Solution {
   public:
   
-    int selections(int index,int n,vector<int>&price){
+    int selections(int index,int n,vector<int>&price,vector<vector<int>>&dp){
         
         if(index==0){
             return n * price[0];
         }
+        if (dp[index][n]!=-1) return dp[index][n];
         
-        int nonTake = 0+selections(index-1,n,price);
+        int nonTake = 0+selections(index-1,n,price,dp);
         int take = INT_MIN;
         int rodLength = index+1;
         if(rodLength<=n)
-            take = price[index]+selections(index,n-rodLength,price);
-        return max(take,nonTake);
+            take = price[index]+selections(index,n-rodLength,price,dp);
+        dp[index][n] = max(take,nonTake);
+        return dp[index][n];
     }
     int cutRod(vector<int> &price) {
-        return selections(price.size()-1,price.size(),price);
+        int size = price.size();
+        vector<vector<int>>dp(size,vector<int>(size+1,-1));
+        return selections(price.size()-1,price.size(),price,dp);
         
     }
 };
